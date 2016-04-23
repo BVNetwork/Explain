a22cc0bbc5f9903cc3999af9ee95e4635a04d4d4
Per Magne Skuseth <permagne@bvnetwork.com>
2016-04-23 13:04:42 +0200

    We've got some explaining to do :sunglasses:

---
+++ src/ExplainCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using EPiServer.Find;
+using EPiServer.Find.Api;
+using EPiServer.Find.Connection;
+using EPiServer.Find.Helpers;
+using Newtonsoft.Json;
+using EPiServer.Find.Json;
+
+namespace BVNetwork.Explain
+{
+    public class ExplainCommand : Command
+    {
+        private readonly string _index;
+
+        public ExplainCommand(ICommandContext commandContext, string index) : base(commandContext)
+        {
+            _index = index;
+        }
+
+        public object Explain(ISearch search, string type, string id)
+        {
+            IJsonRequest request = CommandContext.RequestFactory.CreateRequest(GetUrl(type, id), HttpVerbs.Post, ExplicitRequestTimeout);
+            try
+            {
+                var context = new SearchContext();
+                search.ApplyActions(context);
+                string requestBody = CommandContext.Serializer.Serialize(context.RequestBody);
+                request.WriteBody(requestBody);
+                using (var stringReader = new StringReader(request.GetResponse()))
+                {
+                    object explain = CommandContext.Serializer.Deserialize<object>(new JsonTextReader(stringReader));
+                    return explain;
+                }
+            }
+            catch (WebException ex)
+            {
+                string message = ex.Message;
+                if (ex.Response.IsNotNull())
+                {
+                    string end = new StreamReader(ex.Response.GetResponseStream(), Encoding.UTF8).ReadToEnd();
+                    message = message + Environment.NewLine + end;
+                }
+                throw new WebException(message, ex);
+            }
+        }
+
+        private string GetUrl(string type, string id)
+        {
+            return string.Format("{0}{1}/{2}/{3}/_explain", GetServerUrl(), _index, type, id);
+        }
+    }
+}
---
+++ src/ExplainExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Find;
+
+namespace BVNetwork.Explain
+{
+    public static class ExplainExtensions
+    {
+        //public static object Explain(this IClient client, ISearch search, IContent content)
+        //{
+        //    var type = content.GetOriginalType().FullName.Replace('.', '_');
+        //    var indexId = content.GetIndexId();
+        //    return client.NewCommand(context => new ExplainCommand(context, client.DefaultIndex)).Explain(search, type, indexId);
+        //}
+
+        public static object Explain(this IClient client, ISearch search, string type, string id)
+        {
+            return client.NewCommand(context => new ExplainCommand(context, client.DefaultIndex)).Explain(search, type, id);
+        }
+    }
+}
